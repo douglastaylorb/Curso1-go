d6e8888af1ec9a7187779eab7b2429511a1b19e2
Douglas Taylor <douglastaylor20@gmail.com>
2024-02-14 17:29:50 -0300

    Primeiro Commit

---
+++ go/src/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Douglas"
+	versao := 1.1
+	idade := 32
+	fmt.Println("Olá,", nome, "Sua idade é:", idade)
+	fmt.Println("Este programa está na versão:", versao)
+}
