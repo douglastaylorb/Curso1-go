83b125c63af938058f35fd0f41d74862f9fff685
Douglas Taylor <douglastaylor20@gmail.com>
2024-02-14 17:35:05 -0300

    Alterar versão Hello

--- go/src/hello.go
+++ go/src/hello.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	nome := "Douglas"
-	versao := 1.1
+	versao := 1.2
 	idade := 32
 	fmt.Println("Olá,", nome, "Sua idade é:", idade)
 	fmt.Println("Este programa está na versão:", versao)
